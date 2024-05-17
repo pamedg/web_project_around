8739a8a19e335fcc4558500722c27034db61a898
Pamedg <pameladiazglez2507@gmail.com>
2024-05-17 14:14:56 -0600

    actualizacion175

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const popupProfile = document.querySelector("#popup-profile");
+console.log("popupProfile ->", popupProfile);
+
+addEventListener();
+formElement.addEventListener("submit", handleProfileFormSubmit);
