68cd4976fcd05acba2f20a39b26be8ec6b3a5131
Pamedg <pameladiazglez2507@gmail.com>
2024-05-21 21:22:33 -0600

    actualizacion 2105

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 const popupProfile = document.querySelector("#popup-profile");
 const profileButton = document.querySelector(".profile__bottom");
-const formProfile = document.querySelector("#form.profile");
+const closeProfilePopupButton = document.querySelector("#close-profile-form");
+const formProfile = document.querySelector("#form-profile");
 const inputName = document.querySelector("#input-name");
 const profileName = document.querySelector(".profile__name");
+const profileOccupation = document.querySelector(".profile__occupation");
+const inputAbout = document.querySelector("#input-about");
 
 function handleOpenProfilePopup() {
   popupProfile.classList.add("popup_show");
@@ -13,10 +16,11 @@ function handleCloseProfilePopup() {
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileName.textContent - inputName.value;
+  profileName.textContent = inputName.value;
+  profileOccupation.textContent = inputAbout.value;
   handleCloseProfilePopup();
 }
 
 profileButton.addEventListener("click", handleOpenProfilePopup);
-
+closeProfilePopupButton.addEventListener("click", handleCloseProfilePopup);
 formProfile.addEventListener("submit", handleProfileFormSubmit);
