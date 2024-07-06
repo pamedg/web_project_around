7293f1d3aa725659cffd4caea7fc0910090552e3
Pamedg <pameladiazglez2507@gmail.com>
2024-07-06 09:39:51 -0600

    agregar boton add

--- scripts/index.js
+++ scripts/index.js
@@ -6,12 +6,20 @@ const inputName = document.querySelector("#input-name");
 const profileName = document.querySelector(".profile__name");
 const profileOccupation = document.querySelector(".profile__occupation");
 const inputAbout = document.querySelector("#input-about");
+const popupProfileAdd = document.querySelector("#popup-profile-add");
+const profileButtonAdd = document.querySelector(".profile__bottom-add");
+const closeProfilePopupButtonAdd = document.querySelector(
+  "#close-profile-form-add"
+);
 
 function handleOpenProfilePopup() {
   popupProfile.classList.add("popup_show");
+  popupProfileAdd.classlist.add("popup_show");
 }
+
 function handleCloseProfilePopup() {
   popupProfile.classList.remove("popup_show");
+  popupProfileAdd.classlist.remove("popup_show");
 }
 
 function handleProfileFormSubmit(evt) {
