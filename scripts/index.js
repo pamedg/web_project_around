027c1853d30fca33d07c091af1a5f15ee70f55cf
Pamedg <pameladiazglez2507@gmail.com>
2024-07-08 13:59:54 -0600

    Arreglar close de formularios

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const popupProfile = document.querySelector("#popup-profile");
+const popupAddCard = document.querySelector("#popup-add");
 const profileButton = document.querySelector(".profile__bottom");
 const closeProfilePopupButton = document.querySelector("#close-profile-form");
 const formProfile = document.querySelector("#form-profile");
@@ -6,20 +7,51 @@ const inputName = document.querySelector("#input-name");
 const profileName = document.querySelector(".profile__name");
 const profileOccupation = document.querySelector(".profile__occupation");
 const inputAbout = document.querySelector("#input-about");
-const popupProfileAdd = document.querySelector("#popup-profile-add");
 const profileButtonAdd = document.querySelector(".profile__bottom-add");
 const closeProfilePopupButtonAdd = document.querySelector(
   "#close-profile-form-add"
 );
+const initialCards = [
+  {
+    name: "San Francisco, USA",
+    link: "./images/Sanfrancisco.png",
+  },
+  {
+    name: "Oaxaca, México",
+    link: "./images/Oaxacamexico.png",
+  },
+  {
+    name: "Andalucia, España",
+    link: "./images/AndaluciaEspaña.png",
+  },
+  {
+    name: "New York, USA",
+    link: "./images/newyork.png",
+  },
+  {
+    name: "Venezia, Italia",
+    link: "./images/Veneziaitalia.png",
+  },
+  {
+    name: "Paris, Francia",
+    link: "./images/Parisfrance.png",
+  },
+];
 
 function handleOpenProfilePopup() {
   popupProfile.classList.add("popup_show");
-  popupProfileAdd.classlist.add("popup_show");
 }
 
 function handleCloseProfilePopup() {
-  popupProfile.classList.remove("popup_show");
-  popupProfileAdd.classlist.remove("popup_show");
+  popupProfile.classlist.remove("popup_show");
+}
+
+function handleCloseProfilePopup() {
+  popupAddCard.classlist.add("popup_show");
+}
+
+function handleCloseProfilePopup() {
+  popupAddCard.classList.remove("popup_show");
 }
 
 function handleProfileFormSubmit(evt) {
@@ -30,5 +62,6 @@ function handleProfileFormSubmit(evt) {
 }
 
 profileButton.addEventListener("click", handleOpenProfilePopup);
+profileButtonAdd.addEventListener("click", handleOpenProfilePopup);
 closeProfilePopupButton.addEventListener("click", handleCloseProfilePopup);
 formProfile.addEventListener("submit", handleProfileFormSubmit);
