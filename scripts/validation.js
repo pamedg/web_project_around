993c41b20ad3770d8fdc6bc99451638b392c124c
Pamedg <pameladiazglez2507@gmail.com>
2024-09-18 22:56:33 -0600

    pintar el boton con los valores correctos

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,8 +1,66 @@
+const showInputError = (formElement, inputElement, errorMessage, settings) => {
+  console.log("4. se ejecuta si el input es invalido");
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(settings.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
+};
+
+const hideInputError = (formElement, inputElement, settings) => {
+  console.log("4.1 se ejecuta si el input es valido");
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(settings.inputErrorClass);
+  errorElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement, settings) => {
+  console.log("3,se ejecuta check inputValidity");
+  if (!inputElement.validity.valid) {
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      settings
+    );
+  } else {
+    hideInputError(formElement, inputElement, settings);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const setEventListeners = (formElement, settings) => {
+  console.log("2ejecutamosevenlisteners");
+  const inputList = Array.from(
+    formElement.querySelectorAll(settings.inputSelector)
+  );
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement, settings);
+    });
+  });
+};
+
+const enableValidation = (settings) => {
+  console.log("ejecutamosenable");
+  const formList = Array.from(document.querySelectorAll(settings.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement, settings);
+  });
+};
+
 enableValidation({
   formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__button",
+  inactiveButtonClass: "form__button_disabled",
+  inputErrorClass: "form__input_type_error",
 });
