1d40d9636b6272c585c68add2a0b85fd2f64d83e
Pamedg <pameladiazglez2507@gmail.com>
2025-04-03 09:57:02 -0600

    plugins

--- hello-vite/vite.config.js
+++ hello-vite/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import postcss from "vite-plugin-postcss";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), postcss({ configFilePath: "./my-postcss-config.js" })],
   server: {
     port: 3000,
   },
